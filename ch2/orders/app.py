39ca9ae8a6c15d96607768d0661e4a197acebeb2
Hedinyer Perucho <alucardperucho@gmail.com>
2024-11-27 19:19:32 +0100

    instance of the fasapi application

--- ch2/orders/app.py
+++ ch2/orders/app.py
@@ -1,1 +1,7 @@
 from fastapi import FastAPI
+
+#instance of FastAPI class. this represents our API application
+app = FastAPI(debug=True)
+
+#importing our api module to register in load time our view functions
+from orders.api import api
