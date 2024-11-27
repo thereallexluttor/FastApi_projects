ce9c3c80c5c39b03bdb874b99e2574f683509ad8
Hedinyer Perucho <alucardperucho@gmail.com>
2024-11-27 19:47:56 +0100

    I define an order object to return in our responses.

--- ch2/orders/api/api.py
+++ ch2/orders/api/api.py
@@ -6,3 +6,15 @@ from starlette import status
 
 from orders.app import app
 
+order = {
+    'id' : 'ff0f1355-e821-4178-9567-550dec27a373',
+    'status': "delivered",
+    'created': datetime.utcnow(),
+    'order': [
+        {
+            'product': 'cappuccino',
+            'size': 'medium',
+            'quantity': 1    
+        }
+    ]
+}
