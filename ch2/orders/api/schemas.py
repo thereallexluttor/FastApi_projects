5122347d0458e49978928b6219528f76976f48fb
Hedinyer Perucho <alucardperucho@gmail.com>
2024-11-28 14:52:55 +0100

    size and status schemas declaration

--- ch2/orders/api/schemas.py
+++ ch2/orders/api/schemas.py
@@ -2,3 +2,17 @@ from datetime import datetime
 from enum import Enum
 from typing import List, Optional
 from uuid import UUID
+
+from pydantic import BaseModel, Field, conlist, conint
+
+class size(Enum):
+    small = 'small'
+    medium = 'medium'
+    big = 'big'
+
+class status(Enum):
+    created = 'created'
+    progress = 'progress'
+    cancelled = 'cancelled'
+    dispatched = 'dispatched'
+    delivered = 'delivered'    
